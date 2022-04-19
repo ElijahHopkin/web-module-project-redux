ddd4c3744fb84f5f6d4253dbaa7f162a4a59e0e7
enhopkin@gmail.com <enhopkin@gmail.com>
2022-04-19 14:23:29 -0600

    delete movie action complete

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import { useParams, useHistory } from 'react-router-dom';
+import { deleteMovie } from '../actions/movieActions';
 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const {movies} = props;
+    
+    const {
+        movies,
+        deleteMovie 
+    } = props;
+
     const movie = movies.find(movie=>movie.id===Number(id));
+
+    const deletion = () => {
+        deleteMovie(movie.id)
+        push('/movies')
+    }
     
     return(<div className="modal-page col">
         <div className="modal-dialog">
@@ -39,7 +50,7 @@ const Movie = (props) => {
                         
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete" onClick = {deletion} ><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
@@ -54,4 +65,4 @@ const mapStateToProps = state => {
    }
 }
 
-export default connect (mapStateToProps, {})(Movie);
+export default connect (mapStateToProps, {deleteMovie})(Movie);
